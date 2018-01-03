a37990b3b8bf310b9b31bcd26bbbd6f26d2cc646
Yongjae Lee <yongjae.lee@wrl.onl>
2018-01-03 16:58:20 +0900

    Implement webpage structure

--- eventserver/public/javascripts/main.js
+++ eventserver/public/javascripts/main.js
@@ -7,3 +7,32 @@ window.onload = function() {
 		console.log(msg);
 	});
 }
+
+
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+// var mySidebar = document.getElementById("mySidebar");
+
+function w3_open() {
+    var mySidebar = document.getElementById("mySidebar");
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+    } else {
+        mySidebar.style.display = 'block';
+    }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    var mySidebar = document.getElementById("mySidebar");
+    mySidebar.style.display = "none";
+}
+
+function call_login_modal() {
+    w3_close();
+
+    document.getElementById('loginModalWindow').style.display='block';
+}
+
+function close_login_modal() {
+    document.getElementById('loginModalWindow').style.display='none';
+}
