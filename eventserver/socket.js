59563474d69a1cc04bd438a54bb37e19132405eb
Yongjae Lee <yongjae.lee@wrl.onl>
2018-01-16 18:02:33 +0900

    Edit indention for socket.js

--- eventserver/socket.js
+++ eventserver/socket.js
@@ -2,97 +2,96 @@
 
 
 module.exports = function(http) {
-	
-	var io = require('socket.io')(http);
-	var redis = require('redis');
-	var client = redis.createClient();
-	
-	
-	io.on('connection', function(socket){
-	  	console.log('a user connected');
+  
+  var io = require('socket.io')(http);
+  var redis = require('redis');
+  var client = redis.createClient();
+  
+  
+  io.on('connection', function(socket){
+    console.log('a user connected');
+
+    // device_uuid variable
+    let device_uuid = '';
+    socket.on('vrpn_event', function(msg) {
+
+      if (!msg || !msg.event || !msg.detail) {
+        console.error(`invalid msg!`);
+        return ;
+      }
+
+      // Q: If timestamp is invalid, throw away it? Or create new one in event server?
+      // A message that has invalid timestamp will be dropped.
+      let timestamp = msg.detail.timestamp;
+      if (!timestamp) {
+        return;
+      }
+
+      if (msg.event === 'optitrack_server_on') {
+        const remote_address = socket.request.connection.remoteAddress.split(':')[3];
+        const server_name = msg.detail.server_name || 'noname';
+
+        // device_uuid: ip@server_name
+        device_uuid = remote_address + '@' + server_name;
+      } else {
+        // TODO: publish the msg to authorized client
 
 
-	  	// device_uuid variable
-	  	let device_uuid = '';
-		socket.on('vrpn_event', function(msg) {
+      }
 
-			if (!msg || !msg.event || !msg.detail) {
-				console.error(`invalid msg!`);
-				return ;
-			}
+      // save all msg in redis DB
+      save_in_redis(msg);
+    });
 
-			// Q: If timestamp is invalid, throw away it? Or create new one in event server?
-            // A message that has invalid timestamp will be dropped.
-            let timestamp = msg.detail.timestamp;
-            if (!timestamp) {
-            	return;
-			}
+    socket.on('disconnect', function() {
+      /*
+       * save optitrack_server_off event manually in event server
+       * due to detecting websocket disconnected is only possible by event server
+       *
+       * < message structure >
+       * { event: 'optitrack_server_off',
+       *   detail: {
+       *     timestamp: timestamp,
+       *     server_name: 'dummy_server' } }
+       */
 
-            if (msg.event === 'optitrack_server_on') {
-                const remote_address = socket.request.connection.remoteAddress.split(':')[3];
-                const server_name = msg.detail.server_name || 'noname';
+      let msg = {
+        event: 'optitrack_server_off',
+        detail: {
+          timestamp: Date.now()
+        }
+      }
+      save_in_redis(msg);
 
-                // device_uuid: ip@server_name
-                device_uuid = remote_address + '@' + server_name;
-            } else {
-                // TODO: publish the msg to authorized client
-
-
-            }
-
-            // save all msg in redis DB
-            save_in_redis(msg);
-        });
-
-		socket.on('disconnect', function() {
-			/*
-			 * save optitrack_server_off event manually in event server
-			 * due to detecting websocket disconnected is only possible by event server
-			 *
-			 * < message structure >
-			 * { event: 'optitrack_server_off',
-			 *   detail: {
-			 *   	timestamp: timestamp,
-			 *   	server_name: 'dummy_server' } }
-			 */
-
-			let msg = {
-				event: 'optitrack_server_off',
-				detail: {
-					timestamp: Date.now()
-				}
-			}
-            save_in_redis(msg);
-
-            console.log('user is disconnected');
-		});
+      console.log('user is disconnected');
+    });
 
 
 
-		// save obj data to redis
-		function save_in_redis(obj) {
-			// copy obj
-			var o = Object.assign({}, obj);
+    // save obj data to redis
+    function save_in_redis(obj) {
+      // copy obj
+      var o = Object.assign({}, obj);
 
-			// hmset(key, obj) method do convert each key and value to string by using .toString().
-			// and the method will return an error when a value that is converted to "[object Object]" exist.
-			// so stringify o.detail value by using JSON.stringify
-			for (let key in o) {
-				o[key].toString();
-				if (o[key].toString() === '[object Object]') {
-					o[key] = JSON.stringify(o[key]);
-                }
-            }
+      // hmset(key, obj) method do convert each key and value to string by using .toString().
+      // and the method will return an error when a value that is converted to "[object Object]" exist.
+      // so stringify o.detail value by using JSON.stringify
+      for (let key in o) {
+        o[key].toString();
+        if (o[key].toString() === '[object Object]') {
+          o[key] = JSON.stringify(o[key]);
+        }
+      }
 
-            // has_key: event:device_uuid:timestamp
-            if (device_uuid.length !== 0) {
-                const hash_key = 'event:' + device_uuid + ':' + obj.detail.timestamp;
-                client.hmset(hash_key, o);
+      // has_key: event:device_uuid:timestamp
+      if (device_uuid.length !== 0) {
+        const hash_key = 'event:' + device_uuid + ':' + obj.detail.timestamp;
+        client.hmset(hash_key, o);
 
-                console.log(`saved ${hash_key}`);
-            }
-		}
-	});
-	
-	return io;
+        console.log(`saved ${hash_key}`);
+      }
+    }
+  });
+  
+  return io;
 }
