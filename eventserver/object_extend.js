07d8a15da93eb7e9e7ced0ab51fad196e276f197
Yongjae Lee <yongjae.lee@wrl.onl>
2018-01-22 15:54:27 +0900

    change hash algorithm from md5 to sha-512. (This is more safe than the former against collision)

--- eventserver/object_extend.js
+++ eventserver/object_extend.js
@@ -1,9 +1,13 @@
-var md5 = require('md5');
+var crypto = require('crypto');
 var redis = require('redis');
 
 // hash shortcut
-String.prototype.hash = function() {
-  return md5(this.valueOf());
+String.prototype.hash = function(salt) {
+  salt = salt || 'eventserver-socketio';
+  let hash = crypto.createHash('sha512');
+  hash.update(this.valueOf());
+  hash.update(salt);
+  return hash.digest('base64');
 }
 
 
