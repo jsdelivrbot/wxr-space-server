5269b327afad345e1b9f82a1a9b0080d365e9df9
Yongjae Lee <yongjae.lee@wrl.onl>
2018-01-30 13:44:00 +0900

    Implement basic properties of UserModel and add unit test for UserModel

--- eventserver/models/UserModel.js
+++ eventserver/models/UserModel.js
@@ -8,7 +8,7 @@ const PASSWORD_MINLENGTH = 6;
 /**
  * Model definition of a simple user
  */
-module.exports = nohm.model('User', {
+const UserModel = nohm.model('User', {
 	properties: {
 		id: {
 			type: 'string',
@@ -51,96 +51,53 @@ module.exports = nohm.model('User', {
 	},
 	methods: {
 
-	  /**
-     * Check a given username/password combination for validity.
-     */
-	  login: function (id, password, callback) {
-	    const self = this;
+		validPassword: function (password) {
+			return this.p('password') === password.toString().hash();
+		}
 
-	    if (!id || id === '' || !password || password === '') {
-	    	callback(false);
-	    	return;
-	    }
-
-	    self.load(id, err => {
-				if (err || self.p('password') === password.toString().hash()) {
-					callback(false);
-				} else {
-					callback(true);
-				}
-		  });
-	  }
-
-	//	 /**
-	//		* This function makes dealing with user input a little easier, since we don't want the user to be able to do things on certain fields, like the salt.
-	//		* You can specify a data array that might come from the user and an array containing the fields that should be used from used from the data.
-	//		* Optionally you can specify a function that gets called on every field/data pair to do a dynamic check if the data should be included.
-	//		* The principle of this might make it into core nohm at some point.
-	//		*/
-	//	 fill: function (data, fields, fieldCheck) {
-	//		 var props = {},
-	//			 self = this,
-	//			 doFieldCheck = typeof(fieldCheck) === 'function';
-	//
-	//		 fields = Array.isArray(fields) ? fields : Object.keys(data);
-	//
-	//		 fields.forEach(function (i) {
-	//			 var fieldCheckResult;
-	//
-	//			 if (i === 'salt' || // make sure the salt isn't overwritten
-	//				 ! self.properties.hasOwnProperty(i))
-	//				 return;
-	//
-	//			 if (doFieldCheck)
-	//				 fieldCheckResult = fieldCheck(i, data[i]);
-	//
-	//			 if (doFieldCheck && fieldCheckResult === false)
-	//				 return;
-	//			 else if (doFieldCheck && typeof (fieldCheckResult) !== 'undefined' &&
-	//				 fieldCheckResult !== true)
-	//				 return (props[i] = fieldCheckResult);
-	//
-	//
-	//			 props[i] = data[i];
-	//		 });
-	//
-	//		 this.p(props);
-	//		 return props;
-	//	 },
-	//
-	//	 /**
-	//		* This is a wrapper around fill and save.
-	//		* It also makes sure that if there are validation errors, the salt field is not included in there. (although we don't have validations for the salt, an empty entry for it would be created in the errors object)
-	//		*/
-	//	 store: function (data, callback) {
-	//		 var self = this;
-	//
-	//		 this.fill(data);
-	//		 this.save(function () {
-	//			 delete self.errors.salt;
-	//			 callback.apply(self, Array.prototype.slice.call(arguments, 0));
-	//		 });
-	//	 },
-	//
-	//	 /**
-	//		* Wrapper around fill and valid.
-	//		* This makes it easier to check user input.
-	//		*/
-	//	 checkProperties: function (data, fields, callback) {
-	//		 callback = typeof(fields) === 'function' ? fields : callback;
-	//
-	//		 this.fill(data, fields);
-	//		 this.valid(false, false, callback);
-	//	 },
-	//
-	//	 /**
-	//		* Overwrites nohms allProperties() to make sure password and salt are not given out.
-	//		*/
-	//	 allProperties: function (stringify) {
-	//		 var props = this._super_allProperties.call(this);
-	//		 delete props.password;
-	//		 delete props.salt;
-	//		 return stringify ? JSON.stringify(props) : props;
-	//	 }
 	}
 });
+
+
+/*
+ * Define static methods of UserModel
+ * Defaultly defined methods are:
+ *    load
+ *    find
+ *    findAndLoad
+ *    save
+ *    sort
+ *    subscribe
+ *    subscribeOnce
+ *    unsubscribe
+ *    remove
+ */
+UserModel.findAndLoad = function(id, callback) {
+	var instance = new this();
+
+	instance.findAndLoad({id: id}, (err, users) => {
+		callback(err, users[0]);
+	});
+}
+
+
+UserModel.login = function(id, password, callback) {
+
+	if (!id || id === '' || !password || password === '') {
+		callback(new Error(`id or password is invalid`));
+		return;
+	}
+
+	this.findAndLoad(id, (err, user) => {
+		if (err || !user.validPassword(password)) {
+			callback(err || new Error(`id or password is invalid`));
+		} else {
+			callback(false, user);
+		}
+	});
+
+}
+
+
+
+module.exports = UserModel;
