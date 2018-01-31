6ca279f8eaeb180ee329f14c768cc8a69beaf357
Yongjae Lee <yongjae.lee@wrl.onl>
2018-01-31 20:44:13 +0900

    add object extension for array

--- eventserver/object_extend.js
+++ eventserver/object_extend.js
@@ -3,11 +3,16 @@ var redis = require('redis');
 
 // hash shortcut
 String.prototype.hash = function(salt) {
-  salt = salt || 'eventserver-socketio';
-  let hash = crypto.createHash('sha512');
-  hash.update(this.valueOf());
-  hash.update(salt);
-  return hash.digest('base64');
+	salt = salt || 'eventserver-socketio';
+	let hash = crypto.createHash('sha512');
+	hash.update(this.valueOf());
+	hash.update(salt);
+	return hash.digest('base64');
+}
+
+// array union method
+Array.prototype.union = function(target) {
+	return [...new Set([...this, ...target])];
 }
 
 
@@ -16,7 +21,7 @@ String.prototype.hash = function(salt) {
  */
 // redis.RedisClient.prototype.parse_info = function (callback) {
 //
-//   console.log('parse_info');
-//   callback('ok');
+//	 console.log('parse_info');
+//	 callback('ok');
 //
 // };
