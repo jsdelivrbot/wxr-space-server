992a292af54b2be62b479df42b0e0c72ec4a390f
Yongjae Lee <yongjae.lee@wrl.onl>
2018-02-06 19:11:38 +0900

    Implement UserModel

--- eventserver/models/UserModel.js
+++ eventserver/models/UserModel.js
@@ -1,16 +1,16 @@
 const nohm = require('nohm').Nohm;
+require('../object_extend');
 
 
 const PASSWORD_MINLENGTH = 6;
 
 
-
 /**
  * Model definition of a simple user
  */
-const UserModel = nohm.model('User', {
+const UserModel = nohm.model('UserModel', {
 	properties: {
-		id: {
+		name: {
 			type: 'string',
 			unique: true,
 			validations: [
@@ -18,6 +18,7 @@ const UserModel = nohm.model('User', {
 				'notEmpty'
 			]
 		},
+
 		createdDate: {
 			type: 'timestamp',
 			defaultValue: function () {
@@ -27,6 +28,7 @@ const UserModel = nohm.model('User', {
 				'notEmpty'
 			]
 		},
+
 		password: {
 			load_pure: true, // this ensures that there is no typecasting when loading from the db.
 			type: function (value, key, old) {
@@ -41,6 +43,7 @@ const UserModel = nohm.model('User', {
 					return old;
 				}
 			},
+
 			validations: [
 				'notEmpty',
 				['length', {
@@ -53,49 +56,134 @@ const UserModel = nohm.model('User', {
 
 		validPassword: function (password) {
 			return this.p('password') === password.toString().hash();
+		},
+
+
+		/*
+		 * workspace control helpers
+		 */
+		createWorkspace: function(wsName) {
+			const {WorkspaceModel} = nohm.getModels();
+			const owner = this;
+			return WorkspaceModel.create(owner, wsName);
+		},
+
+		getMyWorkspaces: function() {
+			const {UserModel, WorkspaceModel} = nohm.getModels();
+			return this.getAllLinks('WorkspaceModel', UserModel.RELATION_WORKSPACE_JOINED)
+				.then( ids => WorkspaceModel.propagateInstances(ids) );
+		},
+
+		joinWorkspace: function (ws) {
+			return ws.addMember(this);
+		},
+
+		exitWorkspace: function (ws) {
+			return ws.removeMember(this);
+		},
+
+		getMyRightsIn: function (ws) {
+			return ws.getRightsOf(this);
+		},
+
+		giveRights: function (ws, member, rights) {
+			return ws.setRightsOf(member, rights);
+		},
+
+		ridOfRights: function (ws, member, rights) {
+			return ws.resetRightsOf(member, rights)
+		},
+
+
+
+
+		/*
+		 * device control helpers
+		 */
+		addDevice: function(device) {
+			this.link(device, UserModel.RELATION_DEVICE_HAS);
+			return this._pSave();
+		},
+
+		shareDeviceWith: function(device, user) {
+			// TODO: Checking if the caller is device's owner.
+			user.link(device, UserModel.RELATION_DEVICE_LINKED)
+			return user._pSave()
+			// Added .then chain do nothing for returning undefined
+				.then();
+		},
+
+		getMyDevices: function() {
+			const DeviceModel = nohm.getModels()['DeviceModel'];
+			return this.getAllLinks('DeviceModel', UserModel.RELATION_DEVICE_HAS)
+				.then( ids => DeviceModel.propagateInstances(ids) );
+		},
+
+		getDevices: function() {
+			const DeviceModel = nohm.getModels()['DeviceModel'];
+
+			return this.getAllLinks('DeviceModel', UserModel.RELATIONS_WITH_DEVICE)
+				.then( ids => DeviceModel.propagateInstances(ids) );
+		},
+
+		cutSharingFrom: function(device, user) {
+			// TODO: Checking if the caller is device's owner.
+			user.unlink(device, UserModel.RELATION_DEVICE_LINKED);
+			return user._pSave()
+			// Added .then chain do nothing for returning undefined
+				.then();
+		},
+
+		removeDevice: function(device) {
+			if (this.p('name') === device.p('owner')) {
+				return device.getSharedUser()
+					.then( sharedUsers => {
+						const promisesArray = sharedUsers.map( user => {
+							return this.cutSharingFrom(device, user)
+						});
+						return Promise.all(promisesArray)
+					})
+					.then( () => device.destroy() );
+			}
+			this.unlink(device, UserModel.RELATION_DEVICE_LINKED);
+			return this._pSave();
 		}
 
 	}
 });
 
 
+UserModel.RELATION_WORKSPACE_JOINED = 'viewerForeign';
+UserModel.RELATION_DEVICE_HAS = 'has';
+UserModel.RELATION_DEVICE_LINKED = 'linked';
+UserModel.RELATIONS_WITH_DEVICE = [
+	UserModel.RELATION_DEVICE_HAS,
+	UserModel.RELATION_DEVICE_LINKED
+];
+
+
 /*
  * Define static methods of UserModel
- * Defaultly defined methods are:
- *    load
- *    find
- *    findAndLoad
- *    save
- *    sort
- *    subscribe
- *    subscribeOnce
- *    unsubscribe
- *    remove
  */
-UserModel.findAndLoad = function(id, callback) {
-	var instance = new this();
-
-	instance.findAndLoad({id: id}, (err, users) => {
-		callback(err, users[0]);
-	});
+UserModel.login = function(name, password) {
+	return Promise.resolve()
+		.then( () => {
+			if (!name || name === '' || !password || password === '')
+				return Promise.reject(new Error(`name or password is invalid`));
+		})
+		.then( () => this.findAndLoadByName(name) )
+		.then( user => {
+			if (user.validPassword(password))
+				return Promise.resolve(user);
+			else
+				return Promise.reject(new Error(`name or password is invalid`));
+		});
 }
 
-
-UserModel.login = function(id, password, callback) {
-
-	if (!id || id === '' || !password || password === '') {
-		callback(new Error(`id or password is invalid`));
-		return;
-	}
-
-	this.findAndLoad(id, (err, user) => {
-		if (err || !user.validPassword(password)) {
-			callback(err || new Error(`id or password is invalid`));
-		} else {
-			callback(false, user);
-		}
-	});
-
+UserModel.newUser = function(userInfo) {
+	const user = nohm.factory('UserModel');
+	user.p(userInfo);
+	return user._pSave();
 }
 
 
