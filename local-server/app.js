b248ae3674449a9e88a46a0f732a625df2f846e3
Yongjae Lee <yongjae.lee@wrl.onl>
2018-03-08 18:33:25 +0900

    Get device list linking or linked

--- local-server/app.js
+++ local-server/app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/lib', lib);
 app.use('/user', user);
-app.use('/user/:userId/device', device);
+app.use('/device', device);
 app.use('/workspace', workspace);
 
 // catch 404 and forward to error handler
