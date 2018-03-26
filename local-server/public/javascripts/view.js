d2281ae246755f99f7d7e0a1e943f098e36fac61
Yongjae Lee <yongjae.lee@wrl.onl>
2018-03-26 16:39:07 +0900

    Change view template

--- local-server/public/javascripts/view.js
+++ local-server/public/javascripts/view.js
@@ -2,7 +2,7 @@
 
 window.onload = function() {
 
-  var scale = 2;
+  var scale = 3;
   // socket connect
   var socket = io();
 	console.log('socket connected');
@@ -12,9 +12,9 @@ window.onload = function() {
 	  if (msg.detail.messages.length > 0) {
 		  const message = msg.detail.messages[0];
 		  console.log(message.detail.pos);
-		  options.position.x = message.detail.pos[0] * scale;
-		  options.position.y = message.detail.pos[1] * scale;
-		  options.position.z = message.detail.pos[2] * scale;
+		  sphere.position.x = message.detail.pos[0] * scale;
+		  sphere.position.y = message.detail.pos[1] * scale;
+		  sphere.position.z = message.detail.pos[2] * scale;
     }
 	});
 
@@ -24,7 +24,7 @@ window.onload = function() {
   var camera, tick = 0,
     scene, renderer, clock = new THREE.Clock(),
     controls, container,
-    options, spawnerOptions, particleSystem;
+    sphere;
 
 
   init();
@@ -42,49 +42,21 @@ window.onload = function() {
 
     scene = new THREE.Scene();
 
-    // The GPU Particle system extends THREE.Object3D, and so you can use it
-    // as you would any other scene graph component.	Particle positions will be
-    // relative to the position of the particle system, but you will probably only need one
-    // system for your whole scene
-	  var textureLoader = new THREE.TextureLoader();
-    particleSystem = new THREE.GPUParticleSystem( {
-      maxParticles: 250000,
-	    particleNoiseTex: textureLoader.load("/textures/perlin-512.png"),
-	    particleSpriteTex: textureLoader.load("/textures/particle2.png")
-    } );
 
-    scene.add( particleSystem );
+	  var geometry = new THREE.SphereGeometry( 0.1, 32, 32 );
+	  var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+	  sphere = new THREE.Mesh( geometry, material );
+	  scene.add( sphere );
 
-    // options passed during each spawned
 
-    options = {
-      position: new THREE.Vector3(),
-      positionRandomness: .3,
-      velocity: new THREE.Vector3(),
-      velocityRandomness: .5,
-      color: 0xaa88ff,
-      colorRandomness: .2,
-      turbulence: .5,
-      lifetime: 0.5,
-      size: 3,
-      sizeRandomness: 1
-    };
+		// grid helper
+		var size = 60;
+		var divisions = 60;
+		var colorCenterLine = 0x444444;
+		var colorGrid = 0x888888;
 
-    spawnerOptions = {
-      spawnRate: 15000,
-      horizontalSpeed: 1.5,
-      verticalSpeed: 1.33,
-      timeScale: 1
-    };
-
-	// grid helper
-	var size = 60;
-	var divisions = 60;
-	var colorCenterLine = 0x444444;
-	var colorGrid = 0x888888;
-
-	var gridHelper = new THREE.GridHelper( size, divisions, colorCenterLine, colorGrid );
-	scene.add( gridHelper );
+		var gridHelper = new THREE.GridHelper( size, divisions, colorCenterLine, colorGrid );
+		scene.add( gridHelper );
 
     //
 
@@ -118,7 +90,7 @@ window.onload = function() {
 
     requestAnimationFrame( animate );
 
-    var delta = clock.getDelta() * spawnerOptions.timeScale;
+    var delta = clock.getDelta();
 
     tick += delta;
 
@@ -126,24 +98,8 @@ window.onload = function() {
 
     if ( delta > 0 ) {
 
-		/*
-      options.position.x = Math.sin( tick * spawnerOptions.horizontalSpeed ) * 20;
-      options.position.y = Math.sin( tick * spawnerOptions.verticalSpeed ) * 10;
-      options.position.z = Math.sin( tick * spawnerOptions.horizontalSpeed + spawnerOptions.verticalSpeed ) * 5;
-*/
-		
-      for ( var x = 0; x < spawnerOptions.spawnRate * delta; x++ ) {
-
-        // Yep, that's really it.	Spawning particles is super cheap, and once you spawn them, the rest of
-        // their lifecycle is handled entirely on the GPU, driven by a time uniform updated below
-
-        particleSystem.spawnParticle( options );
-
-      }
 
     }
-
-    particleSystem.update( tick );
 
     render();
 
